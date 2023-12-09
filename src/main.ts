d8fc67eb275886952445ec6247374ca226ed4e19
christian-bayata <franksagie1@gmail.com>
2023-12-09 18:29:53 +0100

    setup main file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<string>('PORT');
+  app.setGlobalPrefix('api/v1');
+
+  await app.listen(port, () => logger.log(`App running on Port: ${port}`));
 }
 bootstrap();
