5202f8ea57399cb3c657b4a5bced14b7844828b5
christian-bayata <franksagie1@gmail.com>
2024-01-05 23:54:52 +0100

    Update - routes bound for only admin user

--- src/DataEntry/data-entry.controller.ts
+++ src/DataEntry/data-entry.controller.ts
@@ -43,7 +43,7 @@ export class DataEntryController {
 
   @UseGuards(JwtAuthGuard, RoleGuard)
   // todo ---- remove 'Role.RWX_DATA_ENTRY_USER' later
-  @Roles(Role.RWX_ADMIN, Role.RWX_DATA_ENTRY_USER)
+  @Roles(Role.RWX_ADMIN)
   @Get('/retrieve')
   async retrieveDataEntry(
     @Req() req: any,
@@ -72,7 +72,7 @@ export class DataEntryController {
   @UseGuards(JwtAuthGuard, RoleGuard)
   @Patch('/actions')
   // todo ---- remove 'Role.RWX_DATA_ENTRY_USER' later
-  @Roles(Role.RWX_ADMIN, Role.RWX_DATA_ENTRY_USER)
+  @Roles(Role.RWX_ADMIN)
   async dataEntryActions(
     @Req() req: any,
     @Res() res: Response,
